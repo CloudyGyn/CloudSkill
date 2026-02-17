bfc2859548fd5201dbafb23d72c3f921c09e7e61
CloudyGyn <sahashubhadeep83@gmail.com>
2026-02-18 00:01:26 +0530

    Add files via upload

---
+++ VPC Networks - Controlling Access/CloudyGyn213.md
@@ -0,0 +1,35 @@
+### 💡 Lab Link: [VPC Flow Logs - Analyzing Network Traffic - GSP212](https://www.skills.google/focuses/1231?catalog_rank=%7B%22rank%22%3A1%2C%22num_filters%22%3A0%2C%22has_search%22%3Atrue%7D&parent=catalog&search_id=72756429)
+
+---
+
+### ⚠️ Disclaimer
+- **This script and guide are provided for  the educational purposes to help you understand the lab services and boost your career. Before using the script, please open and review it to familiarize yourself with Google Cloud services. Ensure that you follow 'Qwiklabs' terms of service and YouTube’s community guidelines. The goal is to enhance your learning experience, not to bypass it.**
+
+### ©Credit
+- **DM for credit or removal request (no copyright intended) ©All rights and credits for the original content belong to Google Cloud [Google Cloud Skill Boost website](https://www.cloudskillsboost.google/)** 🙏
+
+---
+
+### 🚨Copy and run the below commands in Cloud Shell:
+
+```
+curl -LO https://raw.githubusercontent.com/CloudyGyn/CloudSkill/master/VPC%20Networks%20-%20Controlling%20Access
+CloudyGyn213.sh
+sudo chmod +x CloudyGyn213.sh
+./CloudyGyn213.sh
+```
+
+## Congratulations, you're all done with the lab 😄
+
+### 🌐 Join our Community
+
+- <img src="https://github.com/user-attachments/assets/a4a4b767-151c-461d-bca1-da6d4c0cd68a" alt="icon" width="25" height="25"> **Join our [Telegram Channel](https://t.me/CloudyGyn)
+
+
+---
+
+# <img src="https://github.com/user-attachments/assets/6ee41001-c795-467c-8d96-06b56c246b9c" alt="icon" width="45" height="45"> [CloudyGyn](https://www.youtube.com/@CloudyGynOfficial) Don't Forget to like share & subscribe
+
+### Thanks for watching and stay connected :)
+
+---
---
+++ VPC Networks - Controlling Access/CloudyGyn213.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+echo "Enter zone (example: us-central1-a):"
+read ZONE
+export ZONE
+
+echo "Creating Blue & Green servers..."
+
+# Create Blue VM
+gcloud compute instances create blue \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --machine-type=e2-micro \
+  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+  --metadata=enable-oslogin=true \
+  --tags=web-server \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --quiet
+
+# Create Green VM
+gcloud compute instances create green \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --machine-type=e2-micro \
+  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+  --metadata=enable-oslogin=true \
+  --tags=web-server \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --quiet
+
+# Create Firewall Rule
+gcloud compute firewall-rules create allow-http-web-server \
+  --project=$DEVSHELL_PROJECT_ID \
+  --direction=INGRESS \
+  --priority=1000 \
+  --network=default \
+  --allow=tcp:80,icmp \
+  --source-ranges=0.0.0.0/0 \
+  --target-tags=web-server \
+  --quiet
+
+# Create Test VM
+gcloud compute instances create test-vm \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --machine-type=e2-micro \
+  --subnet=default \
+  --quiet
+
+# Create Service Account
+gcloud iam service-accounts create network-admin \
+  --description="Service account for Network Admin role" \
+  --display-name="Network-admin" \
+  --quiet
+
+# Assign Network Admin Role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=serviceAccount:network-admin@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+  --role=roles/compute.networkAdmin \
+  --quiet
+
+# Create Key
+gcloud iam service-accounts keys create credentials.json \
+  --iam-account=network-admin@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+  --quiet
+
+# Setup Blue Server
+cat > bluessh.sh <<'EOF'
+sudo apt-get update -y
+sudo apt-get install nginx-light -y
+sudo sed -i '14c\<h1>Welcome to the blue server!</h1>' /var/www/html/index.nginx-debian.html
+sudo systemctl restart nginx
+EOF
+
+gcloud compute scp bluessh.sh blue:/tmp --zone=$ZONE --quiet
+gcloud compute ssh blue --zone=$ZONE --quiet --command="bash /tmp/bluessh.sh"
+
+# Setup Green Server
+cat > greenssh.sh <<'EOF'
+sudo apt-get update -y
+sudo apt-get install nginx-light -y
+sudo sed -i '14c\<h1>Welcome to the green server!</h1>' /var/www/html/index.nginx-debian.html
+sudo systemctl restart nginx
+EOF
+
+gcloud compute scp greenssh.sh green:/tmp --zone=$ZONE --quiet
+gcloud compute ssh green --zone=$ZONE --quiet --command="bash /tmp/greenssh.sh"
+
+echo "Setup Complete ✅"
