ca2d85d9fa6ac3a54899b345d129012a4a94c3da
CloudyGyn <sahashubhadeep83@gmail.com>
2026-03-08 00:52:46 +0530

    Add bucket creation and IAM role management steps

--- Cloud IAM Qwik Start/CloudyGyn064.sh
+++ Cloud IAM Qwik Start/CloudyGyn064.sh
@@ -3,20 +3,24 @@
 YELLOW_COLOR="\033[1;33m"
 NO_COLOR="\033[0m"
 
-read -p "${YELLOW_COLOR}Enter USERNAME 2 (email): ${NO_COLOR}" USERNAME_2
-
 BUCKET_NAME="${DEVSHELL_PROJECT_ID}-bucket"
 
+echo -e "${YELLOW_COLOR}Creating bucket...${NO_COLOR}"
 gsutil mb -l us -b on gs://$BUCKET_NAME
 
 echo "Subscribe to CloudyGyn" > sample.txt
 
+echo -e "${YELLOW_COLOR}Uploading file...${NO_COLOR}"
 gsutil cp sample.txt gs://$BUCKET_NAME
 
+echo -e "${YELLOW_COLOR}Removing viewer role...${NO_COLOR}"
 gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
 --member="user:$USERNAME_2" \
 --role="roles/viewer"
 
+echo -e "${YELLOW_COLOR}Adding storage.objectViewer role...${NO_COLOR}"
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
 --member="user:$USERNAME_2" \
 --role="roles/storage.objectViewer"
+
+echo -e "${YELLOW_COLOR}Completed!${NO_COLOR}"
